a72250564c9086199553ca13e7dc3ce7d1ca4858
gK <kwesi@kwesi.info>
2023-03-27 19:56:13 -0400

    linking directly to uid now

--- backend/app/routes/bot.py
+++ backend/app/routes/bot.py
@@ -14,7 +14,7 @@ def execute():
 	payload = json.loads(request.get_data().decode())
 	bot_logger.info(f"Received a payload to work with {payload}")
 	resy_active_res_task = dict(filter(lambda key: key[0] in task_handler.res_task_keys, payload.items()))
-	resy_active_res_task['token'] = account_handler.get_resy_token(account_handler.get_user_id(payload['email']))
+	resy_active_res_task['token'] = account_handler.get_resy_token(payload['uid'])
 	resp = task_handler.execute_task(resy_active_res_task, resy_client)
 	bot_logger.info("Attempted to execute a reservation request, {status}".format(status=resp.status_code))
 	return resp
